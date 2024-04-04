ac6a513bc30710087079f7013676d00a8a14af71
fidanbb <fidanbb@code.edu.az>
2024-04-04 19:57:31 +0400

    added GetAllMethod for Category

---
+++ RealEstate_Dapper_Api/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICatogoryRepository _catogoryRepository;
+
+        public CategoriesController(ICatogoryRepository catogoryRepository)
+        {
+            _catogoryRepository = catogoryRepository;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var values=await _catogoryRepository.GetAllCategoryAsync();
+
+            return Ok(values);
+        }
+    }
+}
